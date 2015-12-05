d5a0e0836e0107f72ddd2e98cb1f2d4f92b801d1
Alex Holton <texholt@gmail.com>
2015-12-04 23:32:18 -0600

    Not sure what I did.

---
+++ ShitSoCash/Settings/SSCSettingsViewController.h
@@ -0,0 +1,14 @@
+//
+//  SSCSettingsViewController.h
+//  ShitSoCash
+//
+//  Created by Alex Holton on 12/4/15.
+//  Copyright © 2015 Josh & Alex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SSCSettingsViewController : UIViewController
+
+@end
+
---
+++ ShitSoCash/Settings/SSCSettingsViewController.m
@@ -0,0 +1,47 @@
+//
+//  SSCSettingsViewController.m
+//  ShitSoCash
+//
+//  Created by Alex Holton on 12/4/15.
+//  Copyright © 2015 Josh & Alex. All rights reserved.
+//
+
+#import "SSCSettingsViewController.h"
+
+@import SafariServices;
+
+
+
+@interface SSCSettingsViewController ()
+
+@property (strong, nonatomic) IBOutlet UITextField *HourlyRate;
+
+
+
+@end
+
+@implementation SSCSettingsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (IBAction)WebsiteLink:(UIButton *)sender;
+
+
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+
+
+
+@end
