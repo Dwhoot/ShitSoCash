5179307d04150ab7a20a7661a886d2980d03ace8
Joshua Smith <Home@Joshuas-MBP.(none)>
2015-12-04 19:29:57 -0600

    Initial Commit.

---
+++ ShitSoCash/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ShitSoCash
+//
+//  Created by Joshua Smith on 12/4/15.
+//  Copyright © 2015 Josh & Alex. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
